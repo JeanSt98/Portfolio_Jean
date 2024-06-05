a33c7ca6ad58c21da86253cfb90c51ec534a33f6
Jean Solano <jeansolano98@gmail.com>
2024-06-04 22:56:59 -0500

    Upward button added to all sections

---
+++ js/js.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('.up').click(function(){
+      $('body, html').animate({
+        scrollTop: '0px'
+      }, 100);
+    });
+    
+    $(window).scroll(function(){
+      if( $(this).scrollTop() > 0 ){
+        $('.up').slideDown(300);
+      } else {
+        $('.up').slideUp(300);
+      }
+    });
+    
+    });
